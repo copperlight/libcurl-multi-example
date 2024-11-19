d55e904a6e337759191ad3d1eb235126a8d6bf7b
Matthew Johnson <matthewj@netflix.com>
2024-11-19 16:59:59 -0500

    migrate from conan 1 to conan 2

--- build.sh
+++ build.sh
@@ -1,43 +1,62 @@
 #!/usr/bin/env bash
 
+# usage: ./build.sh [clean|clean --confirm|skiptest]
+
 BUILD_DIR=cmake-build
-# Choose: Debug, Release, RelWithDebInfo and MinSizeRel
-BUILD_TYPE=Debug
+# Choose: Debug, Release, RelWithDebInfo and MinSizeRel. Use Debug for asan checking locally.
+# TODO: fix "Undefined symbols for architecture arm64" when building with Debug on MacOS
+BUILD_TYPE=Release
 
 BLUE="\033[0;34m"
 NC="\033[0m"
 
+if [[ -f /etc/lsb-release ]]; then
+  . /etc/lsb-release
+  if [ "$DISTRIB_CODENAME" = "jammy" ]; then
+    echo "==== fix kernel mmap rnd bits on ubuntu 22.04 to allow asan ===="
+    sudo sysctl vm.mmap_rnd_bits=28
+  fi
+fi
+
 if [[ "$1" == "clean" ]]; then
   echo -e "${BLUE}==== clean ====${NC}"
   rm -rf $BUILD_DIR
-  # remove all packages and binaries from the local cache, to allow swapping between Debug/Release builds
-  conan remove '*' --force
+  if [[ "$2" == "--confirm" ]]; then
+    # remove all packages from the conan cache, to allow swapping between Release/Debug builds
+    conan remove "*" --confirm
+  fi
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  export CC=gcc-11
-  export CXX=g++-11
+  export CC=gcc-13
+  export CXX=g++-13
+fi
+
+if [[ ! -f "$HOME/.conan2/profiles/default" ]]; then
+  echo -e "${BLUE}==== create default profile ====${NC}"
+  conan profile detect
 fi
 
 if [[ ! -d $BUILD_DIR ]]; then
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo -e "${BLUE}==== configure default profile ====${NC}"
-    conan profile new default --detect
-    conan profile update settings.compiler.libcxx=libstdc++11 default
-  fi
-
   echo -e "${BLUE}==== install required dependencies ====${NC}"
   if [[ "$BUILD_TYPE" == "Debug" ]]; then
-    conan install . --build --install-folder $BUILD_DIR --profile ./sanitized
+    conan install . --output-folder=$BUILD_DIR --build="*" --settings=build_type=$BUILD_TYPE --profile=./sanitized
   else
-    conan install . --build=missing --install-folder $BUILD_DIR
+    conan install . --output-folder=$BUILD_DIR --build=missing
   fi
 fi
 
 pushd $BUILD_DIR || exit 1
 
+echo -e "${BLUE}==== configure conan environment to access tools ====${NC}"
+source conanbuild.sh
+
+if [[ $OSTYPE == "darwin"* ]]; then
+  export MallocNanoZone=0
+fi
+
 echo -e "${BLUE}==== generate build files ====${NC}"
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. || exit 1
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE || exit 1
 
 echo -e "${BLUE}==== build ====${NC}"
 cmake --build . || exit 1
--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,16 @@
-from conans import ConanFile
+from conan import ConanFile
 
 
 class SpectatorDConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = (
-        "libcurl/8.4.0",
-        "libuv/1.47.0",
-        "libxml2/2.12.2",
+        "libcurl/8.10.1",
+        "libuv/1.49.2",
+        "libxml2/2.13.4",
     )
-    generators = "cmake"
-    default_options = {}
+    tool_requires = ()
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def configure(self):
         self.options["libcurl"].with_c_ares = True
+        self.options["libcurl"].with_ssl = "openssl"
